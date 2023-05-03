c2102f2f38d13cc07176b057160d3172f6de141a
Shubham Jha <sjha0090@gmail.com>
2023-05-04 03:46:20 +0530

    added files

--- intensive.py
+++ intensive.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 
 # Generate a large array of random numbers
-arr = np.random.rand(300, 300)
+arr = np.random.rand(500, 500)
 
 # Perform a matrix multiplication using NumPy
 start_time = time.time()
